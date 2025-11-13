bef0314709d4ca1c15edeb632e6417dd77f508fe
paullebric <paullebric@gmail.com>
2025-11-13 14:50:37 +0100

    start of project

---
+++ main_code.py
@@ -0,0 +1,7 @@
+import arcade as arc
+import random
+import math
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, BACKGROUND_PATH, BIRD_PATH
+
+
+arc.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
